ca6f83ec7e1619f1e73894181aa4b94c2a7e566e
Anderson Lentz <anderlent@gmail.com>
2019-08-16 10:46:44 -0300

    Showing popular movies (mocked data)

---
+++ ViperMoviesssDB/ViperMoviesssDB/MovieHomeScreen/view/NowPlayingTableViewCell.swift
@@ -0,0 +1,74 @@
+//
+//  NowPlayingTableViewCell.swift
+//  ViperMoviesssDB
+//
+//  Created by Anderson Lentz on 15/08/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+import UIKit
+
+protocol NowPlayingCollectionViewCellDelegate:class {
+    func collectionView(collectioncell:NowPlayingCollectionViewCell?, didTappedInTableview TableCell:NowPlayingTableViewCell)
+    //other delegate methods that you can define to perform action in viewcontroller
+}
+
+class NowPlayingTableViewCell: UITableViewCell, UICollectionViewDelegateFlowLayout {
+    
+    let cellReuseId = "NowPlayingCollectionViewCell"
+    
+    weak var cellDelegate:NowPlayingCollectionViewCellDelegate?
+    var movies:[Movie]?
+    
+    @IBOutlet weak var mCollectionView: UICollectionView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        //TODO: need to setup collection view flow layout
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.itemSize = CGSize(width: 152, height: 308)
+        flowLayout.minimumLineSpacing = 2.0
+        flowLayout.minimumInteritemSpacing = 10.0
+        self.mCollectionView.collectionViewLayout = flowLayout
+        
+        self.mCollectionView.delegate = self
+        self.mCollectionView.dataSource = self
+        
+        //register the xib for collection view cell
+        let cellNib = UINib(nibName: "NowPlayingCollectionViewCell", bundle: nil)
+        self.mCollectionView.register(cellNib, forCellWithReuseIdentifier: cellReuseId)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+   
+    
+}
+
+extension NowPlayingTableViewCell: UICollectionViewDataSource,
+    UICollectionViewDelegate{
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath) as? NowPlayingCollectionViewCell
+        self.cellDelegate?.collectionView(collectioncell: cell, didTappedInTableview: self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies?.count ?? 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseId, for: indexPath) as? NowPlayingCollectionViewCell
+        
+        return cell!
+        
+    }
+    
+}
