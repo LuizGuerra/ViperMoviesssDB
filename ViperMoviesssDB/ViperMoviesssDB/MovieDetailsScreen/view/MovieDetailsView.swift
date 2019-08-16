0abee0ff702055df7ecd502c9d455290049b8530
Anderson Lentz <anderlent@gmail.com>
2019-08-16 17:15:15 -0300

    Implemented movie details screen

--- ViperMoviesssDB/ViperMoviesssDB/MovieDetailsScreen/view/MovieDetailsView.swift
+++ ViperMoviesssDB/ViperMoviesssDB/MovieDetailsScreen/view/MovieDetailsView.swift
@@ -16,6 +16,9 @@ class MovieDetailsView: UIViewController {
     
     private var object : MovieDetailsEntity?
     
+    @IBOutlet weak var mTableView: UITableView!
+    
+    
     override func loadView() {
         // setting the custom view as the view controller's view
         ui.delegate = self
@@ -29,6 +32,13 @@ class MovieDetailsView: UIViewController {
         
         // Informs the Presenter that the View is ready to receive data.
         presenter.fetch(objectFor: self)
+        
+        let movieDetailsHeaderNib = UINib.init(nibName: "MovieDetailsHeader", bundle: Bundle.main)
+        self.mTableView.register(movieDetailsHeaderNib, forHeaderFooterViewReuseIdentifier: "movieDetailsHeaderID")
+    
+    
+        self.mTableView.delegate = self
+        self.mTableView.dataSource = self
     }
     
 }
@@ -54,4 +64,52 @@ extension MovieDetailsView: MovieDetailsViewUIDataSource {
     }
     
     // Pass the pre-defined object to the dataSource.
+}
+
+extension MovieDetailsView: UITableViewDelegate,
+UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0{
+            return 0
+        }else{
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "overviewCell") as! MovieOverviewTableViewCell
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 1{
+            return "Overview"
+        }
+        
+        return nil
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 194
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        if section == 0{
+            let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "movieDetailsHeaderID") as! NowPlayingViewHeader
+            return headerView
+        }else {
+            return nil
+        }
+    }
+    
+    
 }
